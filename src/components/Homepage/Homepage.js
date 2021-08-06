aa65d3f60c7d7c5d05034c6d5ace67ed9f062fc5
Zymryte Kabashi <zymryte.kabashi@inspire11.com>
2021-08-06 12:15:51 +0200

    Added scroll animation effects on the homepage.

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import './Homepage.css';
+import { useEffect } from 'react';
 import { Grid, Icon, Image, Button } from 'semantic-ui-react';
 import Goxl from '../../assets/images/g-goxl@2x.jpg';
 import London from '../../assets/images/london@2x.jpg';
@@ -6,16 +7,21 @@ import Pilot from '../../assets/images/pilot@2x.jpg';
 import Helicopter from '../../assets/images/helicopter.jpg';
 import City from '../../assets/images/city.jpg';
 import Work from '../../assets/images/work.jpg';
-
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const Homepage = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, [])
+
     return (
         <>
             <div className="parallax">
                 <Grid padded className="hero-section" stackable>
                     <Grid.Row >
                         <Grid.Column columns={1} className="hero-content">
-                            <h3>Precision <span>& Excellence </span> <span>in the Skies </span> </h3>
+                            <h3 data-aos='fade-up'>Precision <span>& Excellence </span> <span>in the Skies </span> </h3>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
@@ -33,7 +39,7 @@ const Homepage = () => {
                         </Grid.Column>
 
                     </Grid.Row>
-                    <Grid.Row columns={2}>
+                    <Grid.Row columns={2} data-aos='fade-up'>
                         <Grid.Column>
                             <p>Excel Helicopters provide charter flights from any UK. location. Whether business
                                 or pleasure, helicopter charter or private helicopter hire is the most time-efficient
@@ -48,16 +54,16 @@ const Homepage = () => {
             </div>
             <Grid>
                 <Grid.Row className="images">
-                    <Image src={Goxl} alt='' size="medium" className="goxl" />
-                    <Image src={Pilot} alt='' size="medium" className="pilot" />
-                    <Image src={London} alt='' size="medium" className="london" />
+                    <Image src={Goxl} alt='' size="medium" className="goxl" data-aos='fade-right' />
+                    <Image src={Pilot} alt='' size="medium" className="pilot" data-aos='zoom-out' />
+                    <Image src={London} alt='' size="medium" className="london" data-aos='fade-left' />
                 </Grid.Row>
 
             </Grid>
             <Grid className="about-section" stackable padded >
                 <Grid.Row columns={3} >
-                    <Grid.Column className="about-content" >
-                        <div>
+                    <Grid.Column className="about-content"  >
+                        <div data-aos='slide-right'>
                             <h4>Business Charter</h4>
                             <div className='inner'></div>
 
@@ -68,7 +74,7 @@ const Homepage = () => {
                 </Grid.Row>
                 <Grid.Row columns={1}  >
                     <Grid.Column className="about-content">
-                        <div>
+                        <div data-aos='slide-left'>
                             <h4>Private Hire</h4>
                             <div className='inner'></div>
 
@@ -79,7 +85,7 @@ const Homepage = () => {
                 </Grid.Row>
                 <Grid.Row columns={3}>
                     <Grid.Column className="about-content" columns={3}>
-                        <div>
+                        <div data-aos='slide-right'>
                             <h4>Airports Transfers </h4>
                             <div className='inner'></div>
 
@@ -89,13 +95,13 @@ const Homepage = () => {
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row className="gallery" columns={2}>
-                    <Icon name="instagram" size="huge" />
+                    <Icon name="instagram" size="huge" data-aos='zoom-in' />
                     <Image.Group className='gallery-images' size='medium'>
+                        <Image src={Helicopter} alt='' data-aos='zoom-in'></Image>
+                        <Image src={City} alt='' data-aos='zoom-in' ></Image>
+                        <Image src={Work} alt='' data-aos='zoom-in'></Image>
+                        <Image src={Helicopter} alt='' data-aos='zoom-in' ></Image>
 
-                        <Image src={Helicopter} alt='' ></Image>
-                        <Image src={City} alt='' ></Image>
-                        <Image src={Work} alt='' ></Image>
-                        <Image src={Helicopter} alt='' ></Image>
                     </Image.Group>
                 </Grid.Row>
             </Grid>
